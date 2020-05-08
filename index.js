d8b2ba292309ef77fe747cbebec9ca078eafc9d9
eqbit <eqbits@gmail.com>
2020-05-08 19:06:46 +0500

    initial

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const sendmail = require('sendmail')();
 
 sendmail({
   from: 'no-reply@eqbit.tech',
-  to: 'yd60@mail.ru',
-  subject: 'Would you go walk today?',
-  html: 'Closer to evening',
+  to: 'eqbit@yandex.ru',
+  subject: 'По работе',
+  html: 'Давай остановимся на твоей цифре',
 }, function(err, reply) {
   console.log(err && err.stack);
   console.dir(reply);
