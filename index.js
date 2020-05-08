9e5652c33e18863f95de23d7daf6cdcf477ff033
eqbit <eqbits@gmail.com>
2020-05-08 20:19:40 +0500

    initial

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const sendmail = require('sendmail')();
+const nodemailer = require('nodemailer');
 
-sendmail({
-  from: 'no-reply@eqbit.tech',
-  to: 'nik@eqbit.tech',
-  subject: 'По работе',
-  html: 'Давай остановимся на твоей цифре',
-}, function(err, reply) {
-  console.log(err && err.stack);
-  console.dir(reply);
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '69caaeec9d9418',
+    pass: '08185ce6c96872'
+  }
+});
+
+const message = {
+  from: 'eqbits@gmail.com', // Sender address
+  to: 'to@email.com',         // List of recipients
+  subject: 'Design Your Model S | Tesla', // Subject line
+  text: 'Have the most fun you can in a car. Get your Tesla today!' // Plain text body
+};
+transport.sendMail(message, function(err, info) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(info);
+  }
 });
