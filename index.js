3ea8ab4e249a1ba6323b5c5728a529b353766e27
eqbit <eqbits@gmail.com>
2020-05-12 13:07:52 +0500

    switched to es5 syntax

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const gmailSend = require('gmail-send');
+var express = require("express");
+var gmailSend = require('gmail-send');
 var cors = require('cors');
-const credentials = require('./config');
+var credentials = require('./config');
 
-const app = express();
+var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const sendEmail = gmailSend({
+var sendEmail = gmailSend({
   user: credentials.user,
   pass: credentials.password,
   to: 'eqbit@yandex.ru',
   subject: 'Новый запрос приложения',
 });
 
-app.listen(3002, () => {
+app.listen(3002, function () {
   console.log("Server running on port 3002");
 });
 
-app.post('/request', (req, res) => {
+app.post('/request', function (req, res) {
   
-  const html = `
+  var html = `
   Запрос приложения с лендинга Omnio<br>
   Имя: ${req.body.name} <br>
   Email: ${req.body.email}`;
   
-  sendEmail({ html }, (error) => {
+  sendEmail({ html }, function (error) {
     if (error) {
       return res.send({ error });
     }
